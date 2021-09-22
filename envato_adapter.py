0af7cbba4f09bbc3dedfac840fbdd26605e40675
Oksana Serhiienko <oksana.serhiienko@ihsmarkit.com>
2021-09-22 19:38:57 +0300

    Small fixes

--- envato_adapter.py
+++ envato_adapter.py
@@ -3,20 +3,20 @@ from utils import *
 from decouple import config
 
 def isUserExists(userName):
-    response = __getUser__(userName)
+    response = __getUser(userName)
 
     data = json_extract(response.json(), 'error')
 
     return len(data) == 0
 
 def getSalesCountForUser(userName):
-    response = __getUser__(userName)
+    response = __getUser(userName)
 
     salesCount = json_extract(response.json(), 'sales').pop()
 
     return int(salesCount)
 
-def __getUser__(userName):
+def __getUser(userName):
     url = f"https://api.envato.com/v1/market/user:{userName}.json"
     headers = {
         "Authorization": f"Bearer {config('ENVATO_KEY')}"
